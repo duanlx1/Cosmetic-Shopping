553805df0aace90e75123931ceb830424d819363
duanlx1 <xuanduan1905@gmail.com>
2018-09-05 13:04:18 +0700

    sss

--- src/assets/js/my.js
+++ src/assets/js/my.js
@@ -2,53 +2,55 @@
   // USE STRICT
   "use strict";
 
-      $(document).ready(function () {
+  $(document).ready(function () {
 
-        $(".selection-1").select2({
-          minimumResultsForSearch: 20,
-          dropdownParent: $('#dropDownSelect1')
-        });
+    $(".selection-1").select2({
+      minimumResultsForSearch: 20,
+      dropdownParent: $('#dropDownSelect1')
+    });
 
-        $(".selection-2").select2({
-          minimumResultsForSearch: 20,
-          dropdownParent: $('#dropDownSelect2')
-        });
+    $(".selection-2").select2({
+      minimumResultsForSearch: 20,
+      dropdownParent: $('#dropDownSelect2')
+    });
 
-        $('.block2-btn-addcart').each(function(){
-          var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
-          $(this).on('click', function(){
-            swal(nameProduct, "is added to cart !", "success");
-          });
-        });
-
-        $('.block2-btn-addwishlist').each(function(){
-          var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
-          $(this).on('click', function(){
-            swal(nameProduct, "is added to wishlist !", "success");
-          });
-        });
-
-
-        var filterBar = document.getElementById('filter-bar');
-
-	    noUiSlider.create(filterBar, {
-	        start: [ 50, 200 ],
-	        connect: true,
-	        range: {
-	            'min': 50,
-	            'max': 200
-	        }
-	    });
-
-	    var skipValues = [
-	    document.getElementById('value-lower'),
-	    document.getElementById('value-upper')
-	    ];
-
-	    filterBar.noUiSlider.on('update', function( values, handle ) {
-	        skipValues[handle].innerHTML = Math.round(values[handle]) ;
-	    });
-
+    $('.block2-btn-addcart').each(function () {
+      var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+      $(this).on('click', function () {
+        swal(nameProduct, "is added to cart !", "success");
       });
+    });
+
+    $('.block2-btn-addwishlist').each(function () {
+      var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+      $(this).on('click', function () {
+        swal(nameProduct, "is added to wishlist !", "success");
+      });
+    });
+
+
+    var filterBar = document.getElementById('filter-bar');
+    if (filterBar) {
+      noUiSlider.create(filterBar, {
+        start: [50, 200],
+        connect: true,
+        range: {
+          'min': 50,
+          'max': 200
+        }
+      });
+
+      var skipValues = [
+        document.getElementById('value-lower'),
+        document.getElementById('value-upper')
+      ];
+
+      filterBar.noUiSlider.on('update', function (values, handle) {
+        skipValues[handle].innerHTML = Math.round(values[handle]);
+      });
+    }
+
+
+  });
 
 })(jQuery);
